533951b3cdcbad00c5f791368f266a93e8c7b89e
aryangupta899 <99213692+aryangupta899@users.noreply.github.com>
2023-11-08 01:26:35 +0530

    Update Chaotic S Box.cpp

--- Chaotic S Box.cpp
+++ Chaotic S Box.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <stdlib.h>
 #include <iostream>
 #include <bitset>
 #include <string>
@@ -9,6 +10,7 @@ using namespace std;
 #define N 100000
 
 
+
 typedef bitset<8> byte;
 typedef bitset<32> word;
 
@@ -17,14 +19,15 @@ const int Nk = 4;  // Nk Represents the number of word s that are input keys
 
 
 byte S_Box[16][16];
+unsigned int sbox[256];
 void Chaotic_SBox()
 {
     double dt = 0.002;
     double dx, dy, dz;
 
     float a = 0.2;  // standard parameter value selected by Rossler
-    float b = 0.1;
-    float c = 5.7;
+    float b = 0.2;
+    float c = 5.6;
 
     double x = 0; // strange attractor with unidentified variables
     double y = 0;
@@ -44,10 +47,13 @@ void Chaotic_SBox()
 
         if(i>200)
         {
-            int x6=(int)(x*1000000)%10;
+            int x3=(int)(x*1000)%10;
+            int x4=(int)(x*10000)%10;
+            int x5=(int)(x*100000)%10;
+            int y3=(int)(y*1000)%10;
+            int y4=(int)(y*10000)%10;
             int y5=(int)(y*100000)%10;
-            int z4=(int)(z*10000)%10;
-            int num=(x6*100+y5*10+z4)%256;
+            int num=(y3*1000+y4*100+x3*10+x4)%256;
             num=abs(num);
             byte b=bitset<8>(num);
             st.insert(b);
@@ -62,6 +68,8 @@ void Chaotic_SBox()
     for(auto it: st)
     {
         S_Box[i][j]=it;
+        unsigned int int_value = it.to_ulong();
+        sbox[i*10+j]=int_value;
         j++;
         if(j==16)
         {
@@ -446,13 +454,6 @@ void decrypt(byte in[4 * 4], word w[4 * (Nr + 1)])
     AddRoundKey(in, key);
 }
 
-/**********************************************************************/
-/*                                                                    */
-/*                              Testing*/
-/*                                                                    */
-/**********************************************************************/
-
-
 void aes(byte plain[16])
 {
     struct timeval start, end;
@@ -473,12 +474,357 @@ void aes(byte plain[16])
     encrypt(plain, w);
 }
 
+
+/**********************************************************************/
+/*                                                                    */
+/*                      SAC and Bic vagera ka code
+/*                                                                    */
+/**********************************************************************/
+
+#define two_power(n) (1u << (n))
+
+#define array_size(a) (sizeof(a) / sizeof(*a))
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+unsigned int array_max(unsigned int *arr, unsigned int length) {
+    unsigned int i;
+    unsigned int max = arr[0];
+    for (i = 1; i < length; ++i) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+unsigned int log_2(unsigned int v) {
+    // https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogObvious
+
+    unsigned int r = 0; // r will be lg(v)
+    while (v >>= 1) // unroll for more speed...
+    {
+        r++;
+    }
+    return r;
+}
+
+unsigned int nbits(unsigned int i) {
+    if (i == 0) return 0;
+    return log_2(i) + 1;
+}
+
+unsigned int parity(unsigned int v) {
+    // https://graphics.stanford.edu/~seander/bithacks.html#ParityParallel
+
+    v ^= v >> 16;
+    v ^= v >> 8;
+    v ^= v >> 4;
+    v &= 0xf;
+    return (0x6996 >> v) & 1u;
+}
+
+unsigned int hamming_weight(unsigned int v) {
+    // http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
+
+    v = v - ((v >> 1) & 0x55555555);                    // reuse input as temporary
+    v = (v & 0x33333333) + ((v >> 2) & 0x33333333);     // temp
+    return ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
+}
+
+double correlation(unsigned int *x, unsigned int *y, unsigned int n) {
+
+    double sx = 0.0;
+    double sy = 0.0;
+    double sxx = 0.0;
+    double syy = 0.0;
+    double sxy = 0.0;
+    int i;
+
+    for(i = 0; i < n; ++i) {
+        double xi = x[i];
+        double yi = y[i];
+
+        sx += xi;
+        sy += yi;
+        sxx += xi * xi;
+        syy += yi * yi;
+        sxy += xi * yi;
+    }
+
+    // covariation
+    double cov = sxy / n - sx * sy / n / n;
+    if (cov == 0) return 0.0;
+
+    // standard error of x
+    double sigmax = sqrt(sxx / n -  sx * sx / n / n);
+    // standard error of y
+    double sigmay = sqrt(syy / n -  sy * sy / n / n);
+
+    // correlation is just a normalized covariation
+    return cov / sigmax / sigmay;
+}
+
+// matrix allocate and print
+
+unsigned int **alloc_uint_matrix(unsigned int m, unsigned int n) {
+    unsigned int **mat;
+    mat = new unsigned int *[m];
+    for (unsigned int i = 0; i < m; ++i) {
+        mat[i] = new unsigned int[n]();
+    }
+    return mat;
+}
+
+void free_uint_matrix(unsigned int **mat, unsigned int m) {
+    for (unsigned int i = 0; i < m; ++i) {
+        delete[] mat[i];
+    }
+    delete[] mat;
+}
+
+void print_uint_matrix(unsigned int **mat, unsigned int m, unsigned int n) {
+
+    unsigned int i, j;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            printf("%2d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+float **alloc_float_matrix(unsigned int m, unsigned int n) {
+    float **mat;
+    mat = new float *[m];
+    for (unsigned int i = 0; i < m; ++i) {
+        mat[i] = new float[n]();
+    }
+    return mat;
+}
+
+void free_float_matrix(float **matrix, unsigned int rows) {
+    for (unsigned int i = 0; i < rows; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+void print_float_matrix(float **mat, unsigned int m, unsigned int n) {
+
+    unsigned int i, j;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            printf("%5.2f ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+unsigned int len = array_size(sbox);
+unsigned int maxi = array_max(sbox, len);
+unsigned int m = log_2(len);
+unsigned int n = nbits(maxi);
+
+
+// ==============================================================================
+
+unsigned int **sbox_differential_table() {
+
+    unsigned int **ddt;
+    unsigned int nrows = two_power(m);
+    unsigned int ncols = two_power(n);
+    unsigned int i, si, di;
+
+    ddt = alloc_uint_matrix(nrows, ncols);
+
+    for (i = 0; i < nrows; ++i) {
+        si = sbox[i];
+        for (di = 0; di < nrows; ++di) {
+            ddt[di][si^sbox[i^di]] += 1;
+        }
+    }
+
+    return ddt;
+
+}
+
+unsigned int **sbox_linear_approx_table() {
+
+    // the LAT is in original values (not subtracted by 2^n / 2)
+
+    unsigned int **lat;
+    unsigned int nrows = two_power(m);
+    unsigned int ncols = two_power(n);
+    unsigned int input, inputMask, outputMask;
+
+    lat = alloc_uint_matrix(nrows, ncols);
+
+    for (input = 0; input < nrows; ++input) {
+        for (inputMask = 0; inputMask < nrows; ++inputMask) {
+            for (outputMask = 0; outputMask < ncols; ++outputMask) {
+                // x1 ^ x2 ^ x3 ...  = parity of x1x2x3...
+                if (parity(input & inputMask) == parity(sbox[input] & outputMask)) {
+                    lat[inputMask][outputMask] += 1;
+                }
+            }
+        }
+    }
+
+    return lat;
+
+}
+
+int sbox_linearity() {
+
+    // https://www.cosic.esat.kuleuven.be/ecrypt/courses/mykonos12/slides/day2/gl.pdf#59
+
+    unsigned int **lat = sbox_linear_approx_table();
+    unsigned int nrows = two_power(m);
+    unsigned int ncols = two_power(n);
+    unsigned int i, j;
+    int val, maxAbs = 0;
+
+    // skip zero input/output mask
+    for (i = 1; i < nrows; ++i) {
+        for (j = 1; j < ncols; ++j) {
+            val = lat[i][j] - (two_power(m) / 2);
+            val=abs(val);
+            if (maxAbs < val)
+                maxAbs = val;
+        }
+    }
+
+    free(lat);
+
+    return maxAbs;
+
+}
+
+int sbox_nonlinearity() {
+
+    // the easy way
+    // - https://github.com/okazymyrov/sbox/blob/master/Sage/CSbox.sage#L842
+    // - http://crypto.stackexchange.com/a/19976
+
+    int lin = sbox_linearity();
+    return two_power(m - 1) - lin;
+
+}
+
+
+float *sbox_ac() {
+    // Assuming m and sbox are defined appropriately
+    int m = 16; // Example value
+    unsigned int two_power_m = 1 << m; // Calculate 2^m
+
+    float *k_aval = new float[m](); // Initialize with zero
+
+    for (int i = 0; i < m; i++) {
+        unsigned int ei = 1 << i; // Calculate 2^i
+        for (unsigned int X = 0; X < two_power_m; ++X) {
+            unsigned int dei = sbox[X] ^ sbox[X ^ ei];
+            int w = __builtin_popcount(dei); // Calculate Hamming weight
+            k_aval[i] += w;
+        }
+    }
+
+    float div = (float)two_power_m * m;
+    for (int i = 0; i < m; ++i) {
+        k_aval[i] /= div;
+    }
+
+    return k_aval;
+}
+
+
+
+float **sbox_sac_matrix() {
+
+    float **sac = alloc_float_matrix(m, n);
+    unsigned int i, j, X, ei, ej, dei;
+
+    for (i = 0; i < m; ++i) {
+        ei = two_power(i);
+        for (j = 0; j < n; ++j) {
+            ej = two_power(j);
+            for (X = 0; X < two_power(m); ++X) {
+                dei = sbox[X] ^ sbox[X ^ ei];
+                sac[i][j] += (dei & ej) >> j; // increment sac[i][j] if bit at position j of dei is set
+            }
+        }
+    }
+
+    float outputLength = (float)two_power(n);
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            sac[i][j] /= outputLength;
+        }
+    }
+
+    return sac;
+
+}
+
+double sbox_bic() {
+
+    unsigned int i, ei, X, dei;
+    unsigned int j, k, ej, ek, dej, dek;
+    unsigned int *aval_vector_j;
+    unsigned int *aval_vector_k;
+    double corr = 0.0;
+    double maxCorr = 0.0;
+
+    // for each input bit position
+    for (i = 0; i < m; ++i) {
+        ei = two_power(i);
+
+        // for each j, k output bit change if j != k
+        for (j = 0; j < n; ++j) {
+            for (k = 0; k < n; ++k) {
+                if (j != k) {
+                    aval_vector_j = new unsigned int[two_power(m)]();
+                    aval_vector_k = new unsigned int[two_power(m)]();
+
+                    // for each possible input
+                    for (X = 0; X < two_power(m); ++X) {
+                        ej = two_power(j);
+                        ek = two_power(k);
+
+                        dei = sbox[X] ^ sbox[X ^ ei];
+                        dej = (dei & ej) >> j;
+                        dek = (dei & ek) >> k;
+
+                        aval_vector_j[X] = dej;
+                        aval_vector_k[X] = dek;
+                    }
+
+                    corr = fabs(correlation(aval_vector_j, aval_vector_k, two_power(m)));
+                    // std::cout << "corr[" << j << "][" << k << "] = " << corr << std::endl;
+                    if (maxCorr < corr)
+                        maxCorr = corr;
+
+                    delete[] aval_vector_j;
+                    delete[] aval_vector_k;
+                }
+            }
+        }
+    }
+
+    return maxCorr;
+}
+
+
 int main()
 {
     Chaotic_SBox();
     Chaotic_Inv_S_Box();
 
-    // Define an array of std::bitset<8> values
+    // Define an array of byte values
     byte plain[16] = {0x32, 0x88, 0x31, 0xe0,
                       0x43, 0x5a, 0x31, 0x37,
                       0xf6, 0x30, 0x98, 0x07,
@@ -488,6 +834,7 @@ int main()
     memcpy(referenceOutput, plain, sizeof(plain));
     aes(referenceOutput);
 
+    /*
     int totalChanges = 0;
 
     // Iterate through each bit position (0 to 7)
@@ -519,6 +866,46 @@ int main()
 
     double avalancheEffect = static_cast<double>(totalChanges) / (8*8* 16);
     cout << "Avalanche Effect: " << avalancheEffect << endl;
+    */
+
+    unsigned int **ddt = sbox_differential_table();
+
+    std::cout << "DDT:" << std::endl;
+    print_uint_matrix(ddt, two_power(m), two_power(n));
+
+    unsigned int **lat = sbox_linear_approx_table();
+
+    std::cout << "LAT:" << std::endl;
+    print_uint_matrix(lat, two_power(m), two_power(n));
+
+    float *ac = sbox_ac();
+
+    std::cout << "AC:" << std::endl;
+    for (int i = 0; i < m; ++i) {
+        std::cout << " " << ac[i];
+    }
+    std::cout << std::endl;
+
+    float **sac = sbox_sac_matrix();
+
+    std::cout << "SAC:" << std::endl;
+    print_float_matrix(sac, m, n);
+
+    // Free the allocated memory
+    free_uint_matrix(lat, two_power(m));
+    free_uint_matrix(ddt, two_power(m));
+    free_float_matrix(sac, m);
+    free(ac);
+
+    double bic = sbox_bic();
+    std::cout << "BIC = " << bic << std::endl;
+
+    int linearity = sbox_linearity();
+    std::cout << "Linearity = " << linearity << std::endl;
+
+    int nonlinearity = sbox_nonlinearity();
+    std::cout << "Non-Linearity = " << nonlinearity << std::endl;
+
 
     return 0;
 }
